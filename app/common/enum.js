2dae7a4cd6be67058332cca6ad1fcddee5bb0feb
lpf <17683808722@163.com>
2024-01-14 11:37:44 +0800

    add: 使用webrtc datachannel 发送控制数据

---
+++ app/common/enum.js
@@ -0,0 +1,44 @@
+/**
+ * 枚举值
+ */
+
+// window name
+const WINDOW_NAME = {
+    Main: "main",
+    Control:"control"
+}
+
+// ipc event name
+const IPC_EVENTS_NAME = {
+    Login: "login",
+    Control: "control",
+    GetSourceId: 'get-source-id',
+    ControlStateChange: "control-state-change",
+    AddStream: "add-stream",
+    Robot: "robot",
+    Offer: "offer",
+    Answer: "answer",
+    Forward: "forward",
+    Candidate:"candidate",
+    ControlCandidate: "control-candidate",
+    CuppetCandidate: "puppet-candidate",
+};
+
+// robot type
+const ROBOT_TYPE = {
+    Mouse: "mouse", // 鼠标事件
+    Keyboard: "key", // 键盘事件
+};
+
+// EventEmitter
+const EVENT_NAMES = {
+    AddStream: "add-stream",
+};
+
+
+module.exports = {
+    WINDOW_NAME,
+    IPC_EVENTS_NAME,
+    ROBOT_TYPE,
+    EVENT_NAMES,
+};
---
+++ app/renderer/src/main/src/utils/peer-puppet.js
@@ -0,0 +1,112 @@
+import { ipcRenderer } from 'electron';
+import { IPC_EVENTS_NAME, WINDOW_NAME, ROBOT_TYPE } from "./enum";
+
+// webrtc连接
+const pc = new window.RTCPeerConnection();
+
+const candidateQueue = []
+
+// 获取桌面共享流
+const getScreenStream = async () =>  {
+    const sourceId = await ipcRenderer.invoke(IPC_EVENTS_NAME.GetSourceId)
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                mandatory: {
+                    chromeMediaSource: "desktop",
+                    chromeMediaSourceId: sourceId,
+                    maxWidth: window.screen.width,
+                    maxHeight: window.screen.height,
+                },
+            },
+        });
+        return stream;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+// 获取candidate
+pc.onicecandidate = (e) => {
+    console.log("candidate", JSON.stringify(e.candidate));
+    // 发送给傀儡端
+    ipcRenderer.send(
+        IPC_EVENTS_NAME.Forward,
+        IPC_EVENTS_NAME.Candidate,
+        WINDOW_NAME.Control,
+        JSON.stringify(e.candidate)
+    );
+}
+
+// 监听datachannel
+pc.ondatachannel = (e) => {
+    console.log('data', e)
+    e.channel.onmessage = (e)  => {
+        console.log('onmessage', e, JSON.parse(e.data))
+       let {type, data} = JSON.parse(e.data)
+        console.log('robot', type, data)
+        if(type === ROBOT_TYPE.Mouse) {
+            data.screen = {
+                width: window.screen.width, 
+                height: window.screen.height
+            }
+        }
+        ipcRenderer.send(IPC_EVENTS_NAME.Robot, type, data)
+    }
+}
+
+// 监听控制端cecandidate，收到之后设置
+ipcRenderer.on(IPC_EVENTS_NAME.Candidate, (e, candidate) => {
+    addIceCandidate(JSON.parse(candidate));
+});
+
+// 设置 addIceCandidate
+const addIceCandidate = async(candidate) => {
+    // 依赖remoteDescription,等其设置成功后才会生效
+    candidate && candidateQueue.push(candidate);
+    if (pc.remoteDescription && pc.remoteDescription.type) {
+        for (let candidate of candidateQueue) {
+            try {
+                const rtcIceCandidate = new RTCIceCandidate(candidate);
+                await pc.addIceCandidate(rtcIceCandidate);
+                candidateQueue.shift();
+            } catch (e) {
+                console.error(e)
+            }
+        }
+    }
+};
+
+// 创建应答
+const createAnswer = async (offer) => {
+    const stream = await getScreenStream();
+    console.log("stream", stream);
+    pc.addStream(stream);
+    await pc.setRemoteDescription(offer);
+    const answer = await pc.createAnswer();
+    console.log("replay answer", JSON.stringify(offer));
+    await pc.setLocalDescription(answer);
+    return { answer: pc.localDescription, stream };
+}
+
+ipcRenderer.on(IPC_EVENTS_NAME.Offer, (e, offer) => {
+    console.log('offer', offer);
+    createAnswer(offer).then(({ answer, stream }) => {
+        console.log("streamstream", stream);
+        const { type, sdp } = answer;
+        // 发起ipc通信，由主进程转发到控制端
+        ipcRenderer.send(
+            IPC_EVENTS_NAME.Forward,
+            IPC_EVENTS_NAME.Answer,
+            WINDOW_NAME.Control, {
+                type,
+                sdp,
+            }
+        );
+    });
+});
+
+
+window.addIceCandidate = addIceCandidate
+window.createAnswer = createAnswer
